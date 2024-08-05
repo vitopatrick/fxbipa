573b63329989f693078b65b47f7b7e90f07b0d00
warlock <fecypher@gmail.com>
2024-08-05 13:20:44 +0100

    change

--- firebase.ts
+++ firebase.ts
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // these are the firebase config files
 const firebaseConfig = {
-  apiKey: "AIzaSyAdOnku_4QmSDtIpyDChnAG28CnlKXHvC4",
-  authDomain: "eightcap-e3774.firebaseapp.com",
-  projectId: "eightcap-e3774",
-  storageBucket: "eightcap-e3774.appspot.com",
-  messagingSenderId: "539698828514",
-  appId: "1:539698828514:web:97465213a08f3244847abb",
+  apiKey: "AIzaSyDesvFcvjexNJavo2yPZiAL_AjUUYmQwMs",
+  authDomain: "bitpay-fddca.firebaseapp.com",
+  projectId: "bitpay-fddca",
+  storageBucket: "bitpay-fddca.appspot.com",
+  messagingSenderId: "275512799478",
+  appId: "1:275512799478:web:d7ae47b046413fa6bb7cd7",
 };
 
 
